f6b612c68a83e3ac353269988d282f45933b21c7
andre <andre.mayala@epitech.eu>
2026-03-27 10:38:27 +0100

    fix: modification de la notation

--- frontend/src/contexts/AuthContextDef.ts
+++ frontend/src/contexts/AuthContextDef.ts
@@ -3,9 +3,17 @@ import { createContext } from 'react';
 export type AuthContextType = {
   isAuthenticated: boolean;
   setAuthenticated: (auth: boolean) => void;
+  isAuthModalOpen: boolean;
+  setAuthModalOpen: (open: boolean) => void;
+  pendingAction: (() => void) | null;
+  setPendingAction: (action: (() => void) | null) => void;
 };
 
 export const AuthContext = createContext<AuthContextType>({
   isAuthenticated: false,
   setAuthenticated: () => {},
+  isAuthModalOpen: false,
+  setAuthModalOpen: () => {},
+  pendingAction: null,
+  setPendingAction: () => {},
 });
