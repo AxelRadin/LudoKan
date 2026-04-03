480ed7dc3efd5fdbe67da2f0865deb426e2119fe
Yasmine Seri <yasmine.seriani@epitech.eu>
2026-04-03 10:36:50 +0200

    modif gamepage

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -415,9 +415,7 @@ export default function GamePage() {
               WebkitTextFillColor: 'transparent',
               letterSpacing: -0.8,
             }}
-          >
-            Ludokan
-          </Typography>
+          ></Typography>
         </Box>
 
         {/* ── HERO : image pleine largeur ── */}
