7012453c9102b780912bcfa69105dc022f08ea09
axelradin <axelradin@gmail.com>
2026-05-12 18:03:45 +0200

    feat(notifications): ferme automatiquement le menu après une action
    Passe la fonction closeMenu au NotificationDropdown pour assurer la fermeture du menu lors d'un clic sur une notification.

--- frontend/src/components/notifications/NotificationIcon.tsx
+++ frontend/src/components/notifications/NotificationIcon.tsx
@@ -66,7 +66,7 @@ export default function NotificationIcon({
         }}
         {...menuProps}
       >
-        <NotificationDropdown />
+        <NotificationDropdown onClose={closeMenu} />
       </Menu>
     </>
   );
