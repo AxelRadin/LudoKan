2c8c2fd0b7130615612e01c7bb8d703239c04e6b
Danny DOMINGUES <danny.domingues@outlook.fr>
2026-05-15 10:12:53 +0200

    chore: format App and header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -69,7 +69,7 @@ const LanguageDropdown: React.FC<{
     setAnchor(null);
     if (isAuthenticated) {
       apiPatch('/api/auth/user/', { language_preference: code }).catch(
-        () => { }
+        () => {}
       );
     }
   };
